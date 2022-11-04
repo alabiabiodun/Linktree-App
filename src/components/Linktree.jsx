980b305ec6cd9f5d6a81670242c8f36c03996ae0
alabiabiodun <alabigeomatic@gmail.com>
2022-11-04 10:17:43 +0100

    feature: Contact button and Avatar share button added

--- src/components/Linktree.jsx
+++ src/components/Linktree.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 export const Linktree = () => {
   return (
-    <div id='container' className='flex flex-col justify-center items-center border border-5 p-10 mt-5 mb-5 lg:ml-60 lg:mr-60 lg:mt-10 lg:mb-5 sm:m-10 md:m-20 rounded-xl w-100 h-100 bg-white-200'>
+    <div id='container' className='flex flex-col justify-center items-center border border-5 p-5 mt-5 mb-5 lg:ml-60 lg:mr-60 lg:mt-10 lg:mb-5 sm:m-10 md:m-20 rounded-xl w-100 h-100 bg-white-200'>
+        <div className='w-full pr-5'>
+            <img src='./icon/icon1.png' alt='share_icon' className='w-10 h-10 float-right hover:scale-[1.2]' />
+          </div>
         <div id='container_profile' className='hover:scale-[1.05]'>
             <img id='profile__img' src='./img/alabiabiodun.jpg' alt='profile_pix' className='w-50 h-40 rounded-full'  />
             <h5 id='twitter' className='text-center text-black-200 font-semibold p-2'>@AlabiAbiodun_O</h5>
@@ -37,18 +40,21 @@ export const Linktree = () => {
                 <h5 className='text-md'>Design Book</h5>
                 <h6 className='text-sm'>(Free design book offered by Zuri)</h6>
               </a>
-            </div> 
+            </div>
+            <div className='text-center w-4/5 h-auto border border-3 border-gray-200 rounded-xl shadow-xl hover:scale-[1.05] p-3 m-2 bg-gray-200'>
+              <a id='contact' href='/' rel="noreferrer">Contact Me</a>
+            </div>
             <div id='footer1' className='flex flex-row justify-center items-center gap-2 mt-10'>
               <img src= './logo/slack.png' alt='slack_logo' className='w-15 h-10' />
               <img src='./logo/github.png' alt='github_logo' className='w-15 h-6'/>
             </div>
-            <div id='footer2' className='flex border-t-2 border-gray-200 mt-20 w-100'>
-              <div className='flex flex-wrap items-center justify-left gap-10 w-100 p-3 m-2'>
+            <div id='footer2' className='flex items-left justify-centerborder-t-2 border-gray-200 mt-20 w-100'>
+              <div className='flex flex-wrap gap-10 w-100 p-3 m-2'>
                 <h5 className='pl-5 pr-5 text-lg font-semibold text-center'>Zuri<small className='text-3xl text-pink-700'>.</small>Internship</h5>
                 <h5 className='pl-5 pr-5 text-md text-center text-gray-500'>HNG Internship 9 Frontend Task</h5>
                 <img src='./logo/zuri.jpg' alt='zuri_logo' className='pl-5 pr-5 w-25 h-10'/>
-              </div>     
-            </div>
+              </div>       
+            </div>                        
         </div>
     </div>
   )
